4c3c8adb304a0c4a5435497a015a885b995aa4c7
cverhaest <cverhaest@cverhaest-ltm5.internal.salesforce.com>
2020-07-21 10:54:34 +0200

    Travel Approval Application step0 Commit

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -s -d 3
+sfdx force:org:create -f config/project-scratch-def.json -s -d 7
 sfdx force:source:push
 sfdx shane:user:password:set -p Salesforce2020TA! -g User -l User --json
 sfdx force:org:display --verbose
